6aef13606c5fd9b837c5be313593a07d6ca8c620
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    sdm845-common: Enable Freeform Multiwindow
    Change-Id: I82965ce0c285ef52bc44b0745ec7c2b0007ec66d

--- sdm845.mk
+++ sdm845.mk
@@ -178,6 +178,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \
